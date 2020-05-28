711ccb5950d84ca2af824893f4b763b6683af0d7
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-05-28 13:55:54 +0100

    fixed some weird string bug?

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from Cython.Build import cythonize
 
 setup(
     name='functions',
-    ext_modules=cythonize("functions.pyx"),
+    ext_modules=cythonize("functions_dev.pyx"),
     zip_safe=False,
 )
 
 setup(
     name='auto_voice_channels',
-    ext_modules=cythonize("auto_voice_channels.pyx"),
+    ext_modules=cythonize("auto_voice_channels_dev.pyx"),
     zip_safe=False,
 )
 
